e15c826159cd370d1bc937fec150bf137378c0cd
Agapeag7 <agapekyb@gmail.com>
2026-07-30 18:34:50 +0200

    add icon

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        <link rel="website icon" type="png" href="ico/spot.png">
+
         <title>Ag7Spot - Trouve près de chez toi</title>
 
         <!-- Polices & Icônes -->
